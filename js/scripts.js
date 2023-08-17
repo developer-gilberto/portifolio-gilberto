84b3c5debeceebaaf2dd13a71564d8881fc0642f
developer-gilberto <developer.gilberto@gmail.com>
2023-08-17 19:59:53 -0300

    section home and menu finish

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-console.log('funcionou');
+// OPEN / CLOSE MENU
+
+let checkboxOpenClose = document.querySelector('#checkbox-open-close');
+let header = document.querySelector('#header');
+let iconBurguer = document.querySelector('#btn-burguer');
+let iconX = document.querySelector('#btn-x');
+
+checkboxOpenClose.addEventListener('click', () => {
+
+    if(checkboxOpenClose.checked == false) {
+
+        header.style.left = '0';
+        iconX.style.display = 'block'
+        iconBurguer.style.display = 'none'
+    } else {
+
+        header.style.left = '-999px';
+        iconX.style.display = 'none'
+        iconBurguer.style.display = 'block'
+    }
+});
+
+// TEMA DARK/LIGHT
+
