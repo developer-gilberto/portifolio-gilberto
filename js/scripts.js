2056a6f0cdfbeceec888152e8cd672e93aac11c0
developer-gilberto <developer.gilberto@gmail.com>
2023-09-05 23:20:13 -0300

    remove iframes section projetos

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 // FUNÇÃO PARA ABRIR/FECHAR MENU MOBILE E ANIMAR BOTÃO HAMBURGUER
 
-const btnMenuBurguer = document.querySelector('#btn-burguer');
 const menuMobile = document.querySelector('.menu');
-const barra1BtnMenuBurguer = document.querySelector('.span-1');
-const barra2BtnMenuBurguer = document.querySelector('.span-2');
-const barra3BtnMenuBurguer = document.querySelector('.span-3');
+const firstBarBtnMenuBurguer = document.querySelector('.span-1');
+const secondBarBtnMenuBurguer = document.querySelector('.span-2');
+const thirdBarBtnMenuBurguer = document.querySelector('.span-3');
 
 function openCloseMenu(){
     menuMobile.classList.toggle('anima-menu');
-    barra1BtnMenuBurguer.classList.toggle('anima-span-1');
-    barra2BtnMenuBurguer.classList.toggle('anima-span-2');
-    barra3BtnMenuBurguer.classList.toggle('anima-span-3');
+    firstBarBtnMenuBurguer.classList.toggle('anima-span-1');
+    secondBarBtnMenuBurguer.classList.toggle('anima-span-2');
+    thirdBarBtnMenuBurguer.classList.toggle('anima-span-3');
 }
 
 // ABRE/FECHA MENU MOBILE
+
+const btnMenuBurguer = document.querySelector('#btn-burguer');
 
 btnMenuBurguer.addEventListener('click', () => {
 
@@ -22,12 +23,23 @@ btnMenuBurguer.addEventListener('click', () => {
 
 // FECHA MENU MOBILE QUANDO CLICAR EM ITENS DO MENU
 
-let itensMenuMobile = document.querySelector('.menu ul');
+let redesSociaisMenuMobile = document.querySelectorAll('.container-sociais .bx');
+let itensMenuMobile = document.querySelectorAll('.itens-menu-mobile');
 
-itensMenuMobile.addEventListener('click', () => {
+redesSociaisMenuMobile.forEach(sociais => {
+    sociais.addEventListener('click', () => {
+        
+        openCloseMenu();
+    });
+});
 
-    openCloseMenu();
-})
+itensMenuMobile.forEach(item => {
+    
+    item.addEventListener('click', () => {
+        
+        openCloseMenu();
+    });
+});
 
 // DARK MODE
 
